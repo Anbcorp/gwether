4ee258bc8676fc8e5610b6912c2e357fd0c550f5
Benoit D <benoit@anbcorp.net>
2025-07-01 23:02:22 +0200

    fix: Handle timezones in mqtt payloads

--- util.go
+++ util.go
@@ -11,12 +11,14 @@ type MqttTime struct {
 
 const expiryDateLayout = "2006-01-02 15:04:05"
 
+var tZ = time.Now().Location()
+
 func (ct *MqttTime) UnmarshalJSON(b []byte) (err error) {
 	s := strings.Trim(string(b), "\"")
 	if s == "null" {
 		ct.Time = time.Time{}
 		return
 	}
-	ct.Time, err = time.Parse(expiryDateLayout, s)
+	ct.Time, err = time.ParseInLocation(expiryDateLayout, s, tZ)
 	return
 }
